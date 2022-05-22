08ef5ae327bc5c82286276f2ca27fea31e4eca0c
Denis Tishuk <denistishuk3@gmail.com>
2022-05-22 13:35:54 +0300

    ПРобный вариант с стримом

--- src/main/java/kz/lakida/javacourse/filesizes/DirectorySizeCalculator.java
+++ src/main/java/kz/lakida/javacourse/filesizes/DirectorySizeCalculator.java
@@ -1,7 +1,10 @@
 package kz.lakida.javacourse.filesizes;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class DirectorySizeCalculator {
 
@@ -9,12 +12,30 @@ public class DirectorySizeCalculator {
      * Посчитать и сложить размер всех файлов в папке.
      * Нужно так же учесть файлы во вложенных папках.
      *
-     * @param path путь для подсчета файлов
+     * @param  args для подсчета файлов
      * @return размер всех файлов в байтах
      * @throws IOException             при ошибках файловой системы
      * @throws PathIsNotAFileException в случае, если переданный путь - это не папка
      */
+    public static void main(String[] args) throws IOException, PathIsNotAFileException {
+        Path path = Paths.get(args[0]);
+        int directorySize = calculateDirectorySize(path);
+
+    }
     public static int calculateDirectorySize(Path path) throws IOException, PathIsNotAFileException {
-        throw new UnsupportedOperationException("Not implemented");
+        try {
+            Path folder = Paths.get(String.valueOf(path)); // Сначала создадим объект папки с помощью метода
+            return (int) Files.walk(folder) // Затем для обхода всей иерархии файлов используем метод walk.
+                    // Он возвращает поток (Stream) из элементов типа Path
+                    .map(Path::toFile) // Эти элементы мы преобразуем в объекты типа File с помощью метода потока map()
+                    .filter(File::isFile) // Затем оставляем только те элементы, которые действительно являются файлами (метод isFile())
+                    .mapToLong(File::length) // Потом у каждого файла берём его размер (метод length())
+                    .sum(); // и в конце все значения суммируем
+        } catch (UnsupportedOperationException e) {
+            throw new UnsupportedOperationException("Not implemented");
+        }catch (IOException e) {
+            throw new IOException("Ошибка файловой системы");
+        }
+        //throw new UnsupportedOperationException("Not implemented");
     }
 }
