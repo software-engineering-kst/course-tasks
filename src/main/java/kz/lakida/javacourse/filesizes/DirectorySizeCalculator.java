d723f50c05c9079eb4da124af688c622c1739b92
Kuanysh-kst <90975457+Kuanysh-kst@users.noreply.github.com>
2022-05-23 17:06:41 +0600

    ex finished

--- src/main/java/kz/lakida/javacourse/filesizes/DirectorySizeCalculator.java
+++ src/main/java/kz/lakida/javacourse/filesizes/DirectorySizeCalculator.java
@@ -1,20 +1,35 @@
 package kz.lakida.javacourse.filesizes;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.NotDirectoryException;
 import java.nio.file.Path;
+
+import static java.nio.file.Files.*;
+import static java.nio.file.Files.isRegularFile;
 
 public class DirectorySizeCalculator {
 
-    /**
-     * Посчитать и сложить размер всех файлов в папке.
-     * Нужно так же учесть файлы во вложенных папках.
-     *
-     * @param path путь для подсчета файлов
-     * @return размер всех файлов в байтах
-     * @throws IOException             при ошибках файловой системы
-     * @throws PathIsNotAFileException в случае, если переданный путь - это не папка
-     */
-    public static int calculateDirectorySize(Path path) throws IOException, PathIsNotAFileException {
-        throw new UnsupportedOperationException("Not implemented");
+    public static void main(String[] args) throws IOException, PathIsNotAFileException {
+
+        int directoryCount = calculateDirectorySize(Path.of(args[0]));
+        System.out.println(directoryCount);
+    }
+
+    //directory это путь директорий
+    public static int calculateDirectorySize(Path directory) throws IOException, PathIsNotAFileException {
+        //Проверка на директорию
+        if (Files.isDirectory(directory)) {
+            //Использую лямбду
+            return Files.walk(directory).
+                    filter(p -> p.toFile().isFile()).
+                    mapToInt(p -> (int) p.toFile().length()).
+                    sum();
+        } else {
+            throw new PathIsNotAFileException("Path " + directory.toAbsolutePath() + " is not a directory");
+        }
     }
 }
--- src/main/java/kz/lakida/javacourse/filesizes/PathIsNotAFileException.java
+++ src/main/java/kz/lakida/javacourse/filesizes/PathIsNotAFileException.java
@@ -1,6 +1,10 @@
 package kz.lakida.javacourse.filesizes;
 
+import java.nio.file.Path;
+
 public class PathIsNotAFileException extends Exception {
+
+    private Path path;
 
     public PathIsNotAFileException(String message) {
         super(message);
