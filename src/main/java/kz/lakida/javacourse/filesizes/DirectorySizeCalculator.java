5cc577c07e5869e5cb7d67129d0fe5183fbb2f9c
denistishuk <denis_tishuk@mail.ru>
2022-05-23 21:14:03 +0300

    Вариант через рекрсию

--- src/main/java/kz/lakida/javacourse/filesizes/DirectorySizeCalculator.java
+++ src/main/java/kz/lakida/javacourse/filesizes/DirectorySizeCalculator.java
@@ -1,7 +1,12 @@
 package kz.lakida.javacourse.filesizes;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Objects;
 
 public class DirectorySizeCalculator {
 
@@ -15,6 +20,29 @@ public class DirectorySizeCalculator {
      * @throws PathIsNotAFileException в случае, если переданный путь - это не папка
      */
     public static int calculateDirectorySize(Path path) throws IOException, PathIsNotAFileException {
-        throw new UnsupportedOperationException("Not implemented");
+        String pathStr = String.valueOf(path);
+        if(!pathStr.contains(".")) {
+            for (File file : Objects.requireNonNull(new File(pathStr).listFiles())) {
+                BasicFileAttributes basicFileAttributes = Files.readAttributes(file.toPath(), BasicFileAttributes.class);
+                if (basicFileAttributes.isRegularFile()) {
+                    sizes += basicFileAttributes.size();
+                } else {
+                    calculateDirectorySize(Path.of(file.toString()));
+                }
+            }
+        } else {
+            throw new PathIsNotAFileException("Path " + pathStr + " is not a directory");
+        }
+        return sizes;
+    }
+    // new UnsupportedOperationException("Not implemented");
+
+
+    private StringBuffer sb;
+    private static int sizes;
+    public static void main(String[] args) throws IOException, PathIsNotAFileException {
+        Path path = Paths.get(args[0]);
+        calculateDirectorySize(path);
+        System.out.println(sizes/1024);
     }
 }
