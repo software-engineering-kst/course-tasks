4caeb94368f0eda1231a91845f3c1951d08e0bbe
Kuanysh-kst <90975457+Kuanysh-kst@users.noreply.github.com>
2022-06-01 16:32:11 +0600

    some

--- src/main/java/kz/lakida/javacourse/networking/ClientSocketDemo.java
+++ src/main/java/kz/lakida/javacourse/networking/ClientSocketDemo.java
@@ -5,27 +5,55 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public class ClientSocketDemo {
+public class ClientSocketDemo implements Runnable {
 
-    public static void main(String[] args) throws IOException {
-        // Инициализация подключения и настройка потоков
-        var socket = new Socket("127.0.0.1", 14001);
-        System.out.println("Started connection");
-        var inputStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        var outputStream = socket.getOutputStream();
+    private final Socket socket;
+    private final String name;
 
-        // Отправляем данные по сети
-        outputStream.write("Hello, world!\r\n".getBytes(StandardCharsets.UTF_8));
-        outputStream.flush();
+    public static void main(String[] args) throws IOException, InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        int threadClient = 0;
 
-        // Получаем ответ от сервера
-        System.out.println("Response from server: " + inputStream.readLine());
-
-        // Закрываем
-        inputStream.close();
-        outputStream.close();
-        socket.close();
+        while (threadClient < 10) {
+            threadClient++;
+            executorService.execute(new ClientSocketDemo("Client " + threadClient));
+        }
+        executorService.shutdown();
     }
 
+    ClientSocketDemo(String name) throws IOException, InterruptedException {
+        this.name = name;
+        this.socket = new Socket("127.0.0.1", 14001);
+        System.out.println("Started connection");
+    }
+
+    @Override
+    public void run() {
+        try (var inputStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             var outputStream = socket.getOutputStream();) {
+
+
+            // Отправляем данные по сети
+            outputStream.write(("Hello from " + name + "!\r\n").getBytes(StandardCharsets.UTF_8));
+            outputStream.flush();
+
+            // Получаем ответ от сервера
+            System.out.println("Response from server: " + inputStream.readLine());
+
+            // Закрываем
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
 }
--- src/main/java/kz/lakida/javacourse/networking/ServerSocketDemo.java
+++ src/main/java/kz/lakida/javacourse/networking/ServerSocketDemo.java
@@ -1,37 +1,67 @@
 package kz.lakida.javacourse.networking;
 
+import javax.naming.ldap.SortKey;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
-public class ServerSocketDemo {
+public class ServerSocketDemo implements Runnable {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        // Открываем порт и ждем подключения
+
+    private final Socket socket;
+
+    public static void main(String[] args)  {
         System.out.println("Server started");
-        var serverSocket = new ServerSocket(14001);
-        var socket = serverSocket.accept();
+
+        try (var serverSocket = new ServerSocket(14001)) {
+            //Пока serverSocket не закрыт
+            while (!serverSocket.isClosed()) {
+                var socket = serverSocket.accept();
+                //Передаю socket через конструктор
+                (new Thread(new ServerSocketDemo(socket))).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Конструктор
+    ServerSocketDemo(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
 
         // Клиент подключился, открываем потоки
         System.out.println("Got connection from " + socket.getInetAddress().toString());
-        var inputStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        var outputStream = socket.getOutputStream();
 
-        // Читаем данные от клиента
-        var line = inputStream.readLine();
-        System.out.println("Request from client: " + line);
+        try (var inputStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+           var outputStream = socket.getOutputStream()) {
 
-        // Возвращаем ответ
-        outputStream.write(line.getBytes(StandardCharsets.UTF_8));
-        outputStream.flush();
+                // Читаем данные от клиента
+                var line = inputStream.readLine();
+                System.out.println("Request from client: " + line);
 
-        // Закрываем
-        outputStream.close();
-        inputStream.close();
-        socket.close();
-        serverSocket.close();
+                // Возвращаем ответ
+                outputStream.write(line.getBytes(StandardCharsets.UTF_8));
+                outputStream.flush();
+
+                // Закрываем
+
+
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
-
 }
