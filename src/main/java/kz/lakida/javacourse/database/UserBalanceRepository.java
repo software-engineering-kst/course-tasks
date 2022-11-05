c9f39f34ca6c86d8f255f41f40d9e6542dd35519
Kuanysh <kuanysh4646@gmail.com>
2022-11-05 22:59:22 +0600

    1

--- src/main/java/kz/lakida/javacourse/database/UserBalanceRepository.java
+++ src/main/java/kz/lakida/javacourse/database/UserBalanceRepository.java
@@ -1,17 +1,41 @@
 package kz.lakida.javacourse.database;
 
+import java.io.IOException;
 import java.math.BigDecimal;
-import java.sql.Connection;
+import java.sql.*;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 public class UserBalanceRepository {
-    public UserBalanceRepository(Connection connection) {
 
+    private final Connection connection;
+
+    public UserBalanceRepository(Connection connection) {
+        this.connection = connection;
     }
 
     public Map<String, BigDecimal> findAllBalancesByUser() {
-        throw new UnsupportedOperationException();
+
+        Map<String, BigDecimal> result = new HashMap<>();
+
+        try {
+            String sql = "SELECT * u.name , sum(a.balances) "
+                    + "FROM users u "
+                    + "LEFT JOIN accounts a ON(a.user_id = u.id) "
+                    + "GROUP BY u.id";
+
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()) {
+                result.put(resultSet.getString(1), resultSet.getBigDecimal(2));
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return result;
     }
 
     public void initTables() {
