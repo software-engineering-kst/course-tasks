3ef8c4a18e018aec0420216ec9c2598d65f13a29
denistishuk <39724916+denistishuk@users.noreply.github.com>
2022-11-06 16:08:38 +0300

    DB_version 2
    уже что то получше.

--- src/main/java/kz/lakida/javacourse/database/UserBalanceRepository.java
+++ src/main/java/kz/lakida/javacourse/database/UserBalanceRepository.java
@@ -16,51 +16,28 @@ public class UserBalanceRepository {
 
     public Map<String, BigDecimal> findAllBalancesByUser() throws SQLException {
         Map<String, BigDecimal> map = new HashMap<>();
-        Statement statement = connection.createStatement();
-        String SQL_SELECT = "select u.name_user,sum(a.balance) from users u left join accounts a on (a.user_id = u.id) group by u.id";
+        Statement statement = this.connection.createStatement();
+        String SQL_SELECT = "select u.name,sum(a.balance) from users u left join accounts a on (a.user_id = u.id) group by u.id";
         ResultSet result = statement.executeQuery(SQL_SELECT);
         while (result.next()){
-           map.put(result.getString(1), result.getBigDecimal(2));
+           map.put(result.getString("name"), result.getBigDecimal("sum"));
         }
+        statement.close();
         return map;
     }
 
     public void initTables() throws SQLException {
-        //connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "password");
-        Statement statement = connection.createStatement();
-        String SQL_CREATE_USER = "create TABLE users(id uuid primary key, name_user VARCHAR ( 255 ) NOT NULL);";
-        String SQL_CREATE_ACCOUNTS = "create TABLE accounts(id uuid primary key, user_id UUID not null references users(id), balance decimal(10,2) not null default 0.0);";
+        Statement statement = this.connection.createStatement();
+        String SQL_CREATE_USER = "create TABLE IF NOT EXISTS users (id uuid primary key, name VARCHAR ( 255 ) NOT NULL);";
+        String SQL_CREATE_ACCOUNTS = "create TABLE IF NOT EXISTS accounts (id uuid primary key, user_id UUID not null references users(id), balance decimal(10,2) not null default 0.0);";
 
-        //statement.execute(SQL_CREATE_USER);
-        //statement.execute(SQL_CREATE_ACCOUNTS);
+        statement.executeUpdate("drop table users CASCADE");
+        statement.executeUpdate("drop table accounts CASCADE");
         statement.executeUpdate(SQL_CREATE_USER);
         statement.executeUpdate(SQL_CREATE_ACCOUNTS);
+        
+        statement.close();
 
-        UUID kuanyshId = UUID.randomUUID();
-        createUser(kuanyshId, "Kuanysh");
-        createAccount(kuanyshId, BigDecimal.valueOf(250000.00));
-        createAccount(kuanyshId, BigDecimal.valueOf(100000.50));
-
-        UUID denisId = UUID.randomUUID();
-        createUser(denisId, "Denis");
-        createAccount(denisId, BigDecimal.valueOf(240000.15));
-        createAccount(denisId, BigDecimal.valueOf(120000.60));
-
-
-    }
-
-    private void createUser(UUID id, String name) throws SQLException {
-        var statement = connection.prepareStatement("INSERT INTO \"users\" (\"id\", \"name\") VALUES (?, ?);");
-        statement.setObject(1, id, Types.OTHER);
-        statement.setString(2, name);
-        statement.executeUpdate();
-    }
-
-    private void createAccount(UUID userId, BigDecimal amount) throws SQLException {
-        var statement = connection.prepareStatement("INSERT INTO \"accounts\" (\"id\", \"user_id\", \"balance\") VALUES (gen_random_uuid(), ?, ?);");
-        statement.setObject(1, userId, Types.OTHER);
-        statement.setObject(2, amount, Types.DECIMAL);
-        statement.executeUpdate();
     }
 
 
