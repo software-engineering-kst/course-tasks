c6a6a201b9346a1824a3ff6f9a8844ad341914e2
denistishuk <39724916+denistishuk@users.noreply.github.com>
2023-02-04 18:53:03 +0300

    Реализовал метод FindMax
    Не получилось сделать именно через Comparable как в задании.
    Получилось довести до рабочего состояния.
    Сделай ролик с пояснением как что надо было делать. Как правильно метод использовать.
    ПОтому что я через Object вызов метода сделал. ХЗ

--- src/main/java/kz/lakida/javacourse/tishuk/genericDemo/FindMax.java
+++ src/main/java/kz/lakida/javacourse/tishuk/genericDemo/FindMax.java
@@ -1,47 +1,11 @@
 package kz.lakida.javacourse.tishuk.genericDemo;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class FindMax {
-/*    class CompareObject<T> implements Comparator<T>{
-
-        @Override
-        public int compare(T o1, T o2) {
-            if (o1 == o2)
-                return 0;
-            else if (o1 > o2)
-                return 1;
-            else
-                return -1;
-        }
-    }*/
-
-    public static <T> T getMax(Iterable<? extends T>  list) {
-        List<Object> newList = new ArrayList<>();
-        for(Object l : list) {
-            newList.add(l);
-        }
-        Collections.sort(newList); 
-        Collections.reverse(newList);
-        return (T) newList.get(0);
+    public static <T> Object findMax (Collection<? extends T> element, Comparator<? super T> comp){
+        var list = new ArrayList<>();
+        list.addAll(element);
+        list.sort((Comparator<? super Object>) comp);
+        return list.get(list.size()-1);
     }
-
-    public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        list.add(4);
-        list.add(6);
-        list.add(2);
-
-        List<String> listSt = new ArrayList<>();
-        listSt.add("Яблоко");
-        listSt.add("Груша");
-        listSt.add("Слива");
-
-        System.out.println("Максимальный элемент числовой = " + getMax(list));
-
-        System.out.println("Максимальный элемент строковый = " + getMax(listSt));
-
-    }
-
-
 }
